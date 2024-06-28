0a58721b5483b391e6c1a802944838f3dece7a53
Walid Baroudi <wa.baroudi9@gmail.com>
2024-06-28 22:30:45 +0200

    MUN-9 - Integrate Summary Script and Separate PDF/Textual Summary Endpoints

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai"
 import { exec as execCb } from "child_process"
 import { promisify } from "util"
+import path from "path"
 
 import dotenv from "dotenv"
 dotenv.config()
@@ -22,13 +23,16 @@ const simpleController = async (req, res) => {
     }
 }
 
-const summarizeController = async (req, res) => {
+const summarizeController = (isPdfReturn) => async (req, res) => {
     const pdf = req.file
     if (!pdf) {
         return res.status(400).json({ message: "No file uploaded" })
     }
+    const pdfName = pdf.filename
     const runScript = async () => {
-        const command = `python ./scripts/summarize.py ${pdf.originalname} ${pdf.path}`
+        const command = `python ../ai_core/python_scripts/document_summarization/summarize_document.py ${pdfName}${
+            isPdfReturn ? "" : " --print"
+        }`
         const { stdout, stderr } = await exec(command)
         if (stderr) {
             throw new Error(stderr)
@@ -39,12 +43,40 @@ const summarizeController = async (req, res) => {
 
     try {
         const scriptResult = await runScript()
-        res.status(200).json({ result: scriptResult })
+
+        if (isPdfReturn) {
+            // Send the file
+            const relativePath =
+                "../ai_core/python_scripts/document_summarization/summarized_docs"
+            const absolutePath = path.resolve(relativePath)
+            const resultFileName = pdfName.replace(".pdf", "_summarized.pdf")
+            const filePath = `${absolutePath}/${resultFileName}`
+            res.sendFile(filePath, (err) => {
+                if (err) {
+                    console.error("File sending failed:", err)
+                    res.status(500).json({
+                        error: "Failed to send file - " + err.message,
+                    })
+                }
+            })
+        } else {
+            res.status(200).json({ summary: scriptResult })
+        }
     } catch (e) {
         console.error(e.message)
         res.status(500).json({ error: e.message })
     } finally {
-        execCb("rm -f ./uploads/originalDoc.pdf") // TODO: read path from config
+        // delete uploaded file
+        execCb(
+            `rm -f ../ai_core/python_scripts/document_summarization/docs/${pdfName}`
+        )
+        // delete summarized file
+        execCb(
+            `rm -f ../ai_core/python_scripts/document_summarization/summarized_docs/${pdfName.replace(
+                ".pdf",
+                "_summarized.pdf"
+            )}`
+        )
     }
 }
 
--- backend/uploadConfig.js
+++ backend/uploadConfig.js
@@ -2,16 +2,16 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (_req, _file, cb) {
-        cb(null, './uploads') // TODO: read from config
+        cb(null, '../ai_core/python_scripts/document_summarization/docs') // TODO: read from config
     },
     filename: function (_req, file, cb) {
-        cb(null, "originalDoc.pdf") // TODO: read from config
+        cb(null, `${Date.now().toString()}-${file.originalname}`) // TODO: read from config
     }
 });
 
 const upload = multer({ 
     storage: storage,
-    fileFilter: function (req, file, cb) {
+    fileFilter: function (_req, file, cb) {
         if (file.mimetype === 'application/pdf') {
             cb(null, true);
         } else {
