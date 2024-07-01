3ac854baf9a9165daa98590d1369954574b784de
Alexander Kranzer <alex98.kranzer@gmail.com>
2024-07-01 16:28:44 +0200

    Fix error getting PDFs on certain pages

--- backend/scraping/scraping.py
+++ backend/scraping/scraping.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.common import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import JavascriptException
 from selenium.webdriver.support.relative_locator import locate_with
 from pathlib import Path
 from zipfile import ZipFile
@@ -43,12 +44,16 @@ class Scraping:
         self.driver.implicitly_wait(5)
         boxes = self.driver.find_elements(By.CLASS_NAME, "mt-4")
         info_locator = locate_with(By.CLASS_NAME, "keyvalue-row").below({By.ID: "sectionheader-info"})
-        rows = self.driver.find_elements(info_locator)
         details = dict()
-        for row in rows:
-            details[row.find_element(By.CLASS_NAME, "keyvalue-key").text] = row.find_element(By.CLASS_NAME, "keyvalue-value").text
+        try:
+            rows = self.driver.find_elements(info_locator)
+            for row in rows:
+                details[row.find_element(By.CLASS_NAME, "keyvalue-key").text] = row.find_element(By.CLASS_NAME, "keyvalue-value").text
+        except JavascriptException:
+            # TODO: fix error for some types of pages (https://risi.muenchen.de/risi/sitzungsvorlage/detail/8322280?dokument=v8482514)
+            pass
 
-        doc_locator = locate_with(By.PARTIAL_LINK_TEXT, ".pdf").below(rows[-1])
+        doc_locator = locate_with(By.PARTIAL_LINK_TEXT, ".pdf")
         docs = self.driver.find_elements(doc_locator)
         links = [(doc.text, doc.get_attribute('href')) for doc in docs]
         return details, links
